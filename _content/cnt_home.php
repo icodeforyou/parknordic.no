f4124c30efd034fd8d4ce92799ed4f0e33349272
Johan Thorstedt <webdev@icode4u.se>
2015-03-02 19:16:36 +0100

    Image map

--- _content/cnt_home.php
+++ _content/cnt_home.php
@@ -7,7 +7,14 @@
       	Gjelder følgende områder: Rosenbergveien 15, Oslo. Marieboesgt. 16, Oslo og Kanalveien, Lillestrøm.
       </p>
     -->
+      <!--
       <a href="/bestille-plass/"><img style="border:none" src="/img/banner_front_park.jpg" alt="Leie 3 måneder for 1 mnd gratis - Gjelder følgende områder: Rosenbergveien 15, Oslo. Marieboesgt. 16, Oslo og Kanalveien, Lillestrøm."></a>
+      -->
+      <img src ="/img/banner_nett_drammen.jpg" width="619" height="189" alt="Bestill plats" usemap="#bestill-map">
+      <map name="bestill-map">
+        <area shape="rect" coords="17,113,149,132" href="https://parknordic-permit.giantleap.no/embedded-user-shop.html#/shop/pre-selected-product/eef13fab-02cb-40cc-975f-b4838d7e94c0" alt="Dr. Hansteinsgt. 20">
+        <area shape="rect" coords="363,112,497,127" href="https://parknordic-permit.giantleap.no/embedded-user-shop.html#/shop/pre-selected-product/0a000f01-dbb5-413c-be6d-8da4203ddc9d" alt="Tollbugt.  26">
+      </map>
     </div>
   </div>
   <div id="lots" class="span4">
